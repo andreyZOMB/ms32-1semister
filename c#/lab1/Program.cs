c7627185dfa7ff51e46693a45a202dadbd51c6cf
andreyZOMB <71494767+andreyZOMB@users.noreply.github.com>
2022-11-27 16:42:13 +0300

    Add files via upload

---
+++ c#/lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1 {
+    public class A {
+        private float a = 2.3f;
+        private float b = 4.5f;
+
+        public float c {
+            get {
+                a *= a;
+                return (++a / b);
+            }
+        }
+    }
+    public class Program {
+        static void Main() {
+            A a1 = new A();
+            Console.WriteLine(a1.c);
+        }
+    }
+}
